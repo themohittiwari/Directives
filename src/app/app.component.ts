73027e9cc43560de9a34faa74940eadec16451fd
Mohit Tiwari <themohittiwari@gmail.com>
2022-06-20 17:09:04 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'App';
+  num = [20, 9, 10, 11, 12, 13, 14, 15];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HoverDirective } from './Directives/hover.directive';
+import { CustomDirective } from './Directives/custom.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HoverDirective,
+    CustomDirective
   ],
   imports: [
     BrowserModule
